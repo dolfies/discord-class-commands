0605371c3e789e3e089079b9e0518521cf8f6318
dolfies <jeyalfie47@gmail.com>
2022-05-01 19:51:43 -0400

    Fix docs building in vscode

--- docs/conf.py
+++ docs/conf.py
@@ -270,7 +270,7 @@ html_static_path = ['_static']
 
 # The name of a javascript file (relative to the configuration directory) that
 # implements a search results scorer. If empty, the default will be used.
-html_search_scorer = '_static/scorer.js'
+html_search_scorer = os.path.join(os.path.dirname(__file__), '_static/scorer.js')
 
 html_js_files = [
   'custom.js',
